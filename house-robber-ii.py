99028908cac90f5c69a89cfc13e0c3433a3c2fce
pradeeppathak9 <pradeeppathak9@gmail.com>
2023-11-22 18:18:13 +0530

    Update house-robber-ii.py

--- house-robber-ii.py
+++ house-robber-ii.py
@@ -2,15 +2,15 @@
 
 def solution(nums):
     # case 1: decided to rob 1st house, that means cannot rob the last house
-    last_state = (0, nums[0]) 
+    last_state_1 = (0, nums[0]) 
     # cannot rob the next house because already robbed 1st house 
-    last_state = (max(last_state), max(last_state))
+    last_state_1 = (max(last_state_1), max(last_state_1))
     
     # loop from 2 to n -1, because cannot robb the last house
     for i in range(2, len(nums) - 1):
-        new_state = (max(last_state), last_state[0] + nums[i])
-        last_state = new_state
-    print(last_state)
+        new_state = (max(last_state_1), last_state[0] + nums[i])
+        last_state_1 = new_state
+    print(last_state_1)
         
     
     # case 2: decided to not rob the 1st house, that means last house can be robbed
@@ -20,7 +20,7 @@ def solution(nums):
         new_state = (max(last_state_2), last_state_2[0] + nums[i])
         last_state_2 = new_state
     print(last_state_2)
-    return max(max(last_state), max(last_state_2))
+    return max(max(last_state_1), max(last_state_2))
     
     
 assert solution([2, 3, 2]) == 3
