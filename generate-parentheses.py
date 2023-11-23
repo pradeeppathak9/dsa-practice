9fe07b1c3aa0b216ebbb31d5f1ab0589a62cb200
pradeeppathak9 <pradeeppathak9@gmail.com>
2023-11-24 03:01:34 +0530

    Create generate-parentheses.py

---
+++ generate-parentheses.py
@@ -0,0 +1,13 @@
+def solution(n, open_braces = 0, s='', result=[]):
+    #     print(n, open_braces, s)
+    if n < 0 or open_braces < 0:
+        return result
+    if n == 0 and open_braces == 0:
+        result.append(s)
+        return result
+
+    result = solution(n - 1, open_braces+1, s + '(', result)
+    result = solution(n, open_braces - 1, s + ')', result)
+    return result
+
+print(solution(3))
