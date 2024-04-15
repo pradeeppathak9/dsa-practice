d8b477dcc4ed1e1094c683ac62d981bce02a85c9
pradeeppathak9 <pradeeppathak9@gmail.com>
2024-04-15 11:50:59 +0530

    Update implement-trie-prefix-tree.py

--- implement-trie-prefix-tree.py
+++ implement-trie-prefix-tree.py
@@ -37,3 +37,42 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+
+
+
+
+
+# class Node: 
+#     def __init__(self):
+#         self.children = {}
+#         self.end = False
+
+# class Trie:
+
+#     def __init__(self):
+#         self.trie = Node()
+
+#     def insert(self, word: str) -> None:
+#         node = self.trie
+#         for c in word:
+#             if c not in node.children:
+#                 node.children[c] = Node()
+#             node = node.children[c]
+#         node.end = True
+
+#     def search(self, word: str) -> bool:
+#         node = self.trie
+#         for c in word:
+#             if c not in node.children:
+#                 return False
+#             node = node.children[c]
+#         return node.end
+
+#     def startsWith(self, prefix: str) -> bool:
+#         node = self.trie
+#         for c in prefix:
+#             if c not in node.children:
+#                 return False
+#             node = node.children[c]
+#         return True
