241ef232df94c8474cc2dcad93fea62d20210bcf
pradeeppathak9 <pradeeppathak9@gmail.com>
2024-04-17 15:50:18 +0530

    Update validate-binary-search-tree.py

--- validate-binary-search-tree.py
+++ validate-binary-search-tree.py
@@ -8,21 +8,34 @@
 #         self.left = left
 #         self.right = right
 
-def solution(root):
-    res = []
-    def dfs(root):
-        if root == None:
-            return
-        dfs(root.left)
-        res.append(root.val)
-        dfs(root.right)
-    dfs(root)
-    for i in range(1, len(res)):
-        if res[i] <= res[i-1]:
-            return False
-    return True
+# def solution(root):
+#     res = []
+#     def dfs(root):
+#         if root == None:
+#             return
+#         dfs(root.left)
+#         res.append(root.val)
+#         dfs(root.right)
+#     dfs(root)
+#     for i in range(1, len(res)):
+#         if res[i] <= res[i-1]:
+#             return False
+#     return True
+
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+#         return solution(root)
+
 
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return solution(root)
+        if root == None:
+            return True
+        if root.left and root.left.val >= root.val:
+            return False
+        if root.right and root.right.val <= root.val:
+            return False
+
+        return self.isValidBST(root.left) and self.isValidBST(root.right)
         
+
