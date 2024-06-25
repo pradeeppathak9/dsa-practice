5d007349c88cc17138a2d16c70e23cbad588ec06
pradeeppathak9 <pradeeppathak9@gmail.com>
2024-06-26 00:34:47 +0530

    Update lowest-common-ancestor-of-a-binary-tree.py

--- medium/lowest-common-ancestor-of-a-binary-tree.py
+++ medium/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 
 
 # Definition for a binary tree node.
