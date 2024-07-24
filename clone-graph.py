220882b78f6d6c2886cb73bdfe0571705506f480
pradeeppathak9 <pradeeppathak9@gmail.com>
2024-07-25 00:43:22 +0530

    Update clone-graph.py

--- clone-graph.py
+++ clone-graph.py
@@ -7,15 +7,15 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
 from typing import Optional
+from collections import deque
+
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         visited = {}
         def dfs(graph):
             if not graph:
                 return None
-            print(graph.val, [el.val for el in visited.keys()])
             if graph in visited:
                 return visited[graph]
                 
@@ -26,7 +26,6 @@ class Solution:
                 node.neighbors.append(visited[neigh] if neigh in visited else dfs(neigh))
             return node
         return dfs(node)
-
 
 
 class Solution:
