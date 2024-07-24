401c710a2eb3cf64e7162cf25c86dc4969e90696
pradeeppathak9 <pradeeppathak9@gmail.com>
2024-07-25 00:50:49 +0530

    Update clone-graph.py

--- clone-graph.py
+++ clone-graph.py
@@ -48,5 +48,47 @@ class Solution:
                         visited[src].neighbors.append(visited[neigh])
                 return visited[graph]
         return bfs(node)
+
+
+
+
+# local implementation
+class Node:
+    val = None
+    neighbors = []
+    
+    def __init__(self, val):
+        self.val = val
+
+if __name__ == '__main__':
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+
+    node1.neighbors = [node2, node3]
+    node2.neighbors = [node1, node3]
+    node3.neighbors = [node2, node1]
+
+    print([ el.val for el in node1.neighbors ] )
+    print([ el.val for el in node2.neighbors ] )
+    print([ el.val for el in node3.neighbors ] )
+
+    visited = {}
+    def dfs(graph):
+        if not graph:
+            return None
+        if graph in visited:
+            return visited[graph]
+            
+        node = Node(graph.val)
+        visited[graph] = node
+    
+        for neigh in graph.neighbors:
+            node.neighbors.append(visited[neigh] if neigh in visited else dfs(neigh))
+        return node
+        
+    clonded_graph = dfs(node1)
+
+
                 
                 
