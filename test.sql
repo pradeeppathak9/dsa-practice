2dcebfdd311f5528244c6e71d43f92ec8cc9fff0
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-01-10 22:37:00 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -16,3 +16,27 @@ Solution:
 select patient_id, patient_name, conditions from Patients
 where conditions like 'DIAB1%'  or  conditions like '% DIAB1%' ;
 
+
+
+Question:
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+
+Solution:
+# Write your MySQL query statement below
+Select Users.user_id, CONCAT(UPPER(SUBSTR(Users.name, 1, 1)), LOWER(SUBSTR(Users.name, 2))) AS name
+From Users
+ORDER BY 
+Users.user_id ASC
