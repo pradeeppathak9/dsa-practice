924fce06d7d2e90870f9fd04cc19d0c4715820c4
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-01-27 22:38:49 +0530

    Update number-of-connected-components-in-an-undirected-graph.py

--- neetcode/graphs/number-of-connected-components-in-an-undirected-graph.py
+++ neetcode/graphs/number-of-connected-components-in-an-undirected-graph.py
@@ -1,1 +1,27 @@
 # https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+
+def solution(n, edges):
+    adj = { i: [] for i in range(n) }
+    for (u, v) in edges:
+        adj[u].append(v)
+        adj[v].append(u)
+
+    visited = set()
+    def dfs(u):
+        if u in visited:
+            return
+        visited.add(u)
+        for nei in adj[u]:
+            dfs(nei)
+        return
+      
+    n_components = 0
+    for i in range(n):
+        print(i, visited)
+        if i not in visited: 
+            dfs(i)
+            n_components += 1
+    return n_components
+    
+
+        
