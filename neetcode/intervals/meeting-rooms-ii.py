384179990952269fbedcd4bbe96ba21b8784c2d8
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-02-03 19:03:12 +0530

    Create meeting-rooms-ii.py

---
+++ neetcode/intervals/meeting-rooms-ii.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/meeting-rooms-ii/
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+        
+        res = count = 0
+        s = e = 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
