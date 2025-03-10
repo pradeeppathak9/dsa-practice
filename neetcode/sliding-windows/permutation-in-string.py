8eb2fb4693323cd499944028ace232224fbd5a45
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-03-10 13:20:47 +0530

    Create permutation-in-string.py

---
+++ neetcode/sliding-windows/permutation-in-string.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/permutation-in-string/
+
+from collections import Counter, defaultdict
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+        
+        if n1 > n2:
+            return False
+
+        s1_counts = [0] * 26
+        s2_counts = [0] * 26
+
+        for i in range(n1):
+            s1_counts[ord(s1[i]) - ord('a')] += 1
+            s2_counts[ord(s2[i]) - ord('a')] += 1
+        
+        if s1_counts == s2_counts:
+            return True
+
+        for i in range(n1, n2):
+            s2_counts[ord(s2[i]) - ord('a')] += 1
+            s2_counts[ord(s2[i-n1]) - ord('a')] -= 1
+            if s1_counts == s2_counts:
+                return True
+                
+        return False
+
+
+
+
+        
