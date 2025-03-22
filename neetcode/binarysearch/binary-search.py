081419a42fd44426f2cd5492b8eadf7ee9ca2ac7
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-03-23 01:29:25 +0530

    Update binary-search.py

--- neetcode/binarysearch/binary-search.py
+++ neetcode/binarysearch/binary-search.py
@@ -2,15 +2,18 @@
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)-1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-            if nums[mid] < target:
-                left = mid+1
-            else:
-                right = mid-1
-        return -1
+        def binarySearch(left, right):
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    return mid
+                if nums[mid] < target:
+                    left = mid+1
+                else:
+                    right = mid-1
+            return -1
+        return binarySearch(0, len(nums)-1)
+            
+        
             
         
