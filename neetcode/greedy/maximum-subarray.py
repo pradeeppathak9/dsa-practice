4af7a86e69da0462226e27db5d157fee4d3addde
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-03-22 19:52:04 +0530

    Create maximum-subarray.py

---
+++ neetcode/greedy/maximum-subarray.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/maximum-subarray/
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = float('-inf')
+        current_sum = 0
+        for num in nums:
+            current_sum += num
+            max_sum = max(max_sum, current_sum)
+            if current_sum < 0:
+                current_sum = 0
+        return max_sum
+
+        
+
+
