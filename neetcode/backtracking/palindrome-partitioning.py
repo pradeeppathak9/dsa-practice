01f353fbe82037bd4f338e263735c7374b84a400
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-03-23 00:21:21 +0530

    Create palindrome-partitioning.py

---
+++ neetcode/backtracking/palindrome-partitioning.py
@@ -0,0 +1,36 @@
+
+# https://leetcode.com/problems/palindrome-partitioning/
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        part = []
+
+        def isPali(s, l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            for j in range(i, len(s)):
+                if isPali(s, i, j):
+                    part.append(s[i: j+1])
+                    dfs(j+1)
+                    part.pop()
+
+        dfs(0)
+        return res
+
+
+
+        
+
+
+
+        
