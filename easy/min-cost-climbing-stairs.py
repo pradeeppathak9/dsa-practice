9c7af76015108c165c920e9a562f9e286f026bbe
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-06-08 00:09:11 +0530

    Update min-cost-climbing-stairs.py

--- easy/min-cost-climbing-stairs.py
+++ easy/min-cost-climbing-stairs.py
@@ -1,5 +1,16 @@
 # https://leetcode.com/problems/min-cost-climbing-stairs/
 
+# DP - Bottom Up
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        for i in range(len(cost) -3, -1, -1 ):
+            cost[i] = cost[i] + min(cost[i+1], cost[i+2])
+        return min(cost[0], cost[1])
+
+
+
+# DFS - Top Down
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         dp = [0] * (len(cost) + 1)
