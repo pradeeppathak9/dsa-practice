c360574a85f227a730a313a32c39ab91abeb3d96
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-06-07 22:49:54 +0530

    Update unique-paths.py

--- medium/unique-paths.py
+++ medium/unique-paths.py
@@ -1,5 +1,19 @@
 # https://leetcode.com/problems/unique-paths/
 
+
+# bottom up solution ~ DP
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+        for i in range(m-1):
+            new_row = [1] * n
+            for j in range(n-2, -1, -1):
+                new_row[j] = new_row[j+1] + row[j]
+            row = new_row
+        return row[0]
+        
+
+# top - down solution ~ DFS + memoization
 def solution(m, n):
     dp =  [[1 for i in range(n)] for j in range(m)] 
     for i in range(m):
