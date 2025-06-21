63e060cc13f95ecf177c6706d1df0621cfcc9eab
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-06-21 21:46:20 +0530

    Update first-missing-positive.py

--- hard/first-missing-positive.py
+++ hard/first-missing-positive.py
@@ -1,5 +1,25 @@
 # https://leetcode.com/problems/first-missing-positive/
 
+# Jun 21 2025
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                nums[i] = 0
+        for i in range(len(nums)):
+            val = abs(nums[i])
+            if  1 <= val <= len(nums):
+                if nums[val-1] > 0: 
+                    nums[val-1] *= -1
+                elif nums[val - 1] == 0:
+                    nums[val-1] = -1 * (len(nums) + 1)
+        for i in range(1, len(nums)+1):
+            if nums[i-1] >= 0:
+                return i
+        return i + 1
+
+
+# Nov 24 2023
 def solution(nums):
     n = len(nums)
     for i in range(n):
