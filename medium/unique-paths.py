d3c80e847cc5c71f725496a3a2a3f9149b29d48a
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-06-25 21:24:36 +0530

    Update unique-paths.py

--- medium/unique-paths.py
+++ medium/unique-paths.py
@@ -14,18 +14,19 @@ class Solution:
         
 
 # top - down solution ~ DFS + memoization
-def solution(m, n):
-    dp =  [[1 for i in range(n)] for j in range(m)] 
-    for i in range(m):
-        for j in range(n):
-            if i > 0 and j > 0 :
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-            elif i > 0:
-                dp[i][j] = dp[i-1][j]
-            elif j > 0:
-                dp[i][j] = dp[i][j-1]
-    print(dp)
-    return dp[-1][-1]
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = {}
+        def dfs(i, j):
+            if i >= m or j >= n:
+                return 0
+            if i == m-1 and j == n-1:
+                return 1
+            if (i, j) in dp:
+                return dp[(i, j)]
+            dp[(i, j)] = dfs(i+1, j) + dfs(i, j+1)
+            return dp[(i, j)]
+        return dfs(0, 0)
 
 
 assert solution(3, 2) == 3
