f4490d48cd9011d9a0789be792aea036edfb2a86
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-06-27 17:14:29 +0530

    Update longest-common-subsequence.py

--- medium/longest-common-subsequence.py
+++ medium/longest-common-subsequence.py
@@ -12,3 +12,27 @@ class Solution:
                 else:    
                     dp[i][j] = max(dp[i+1][j], dp[i][j+1])
         return dp[0][0]
+
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+
+        dp = {}
+        def dfs(i, j):
+            if i == m or j == n:
+                return 0
+
+            if (i, j) in dp:
+                return dp[(i, j)]
+
+            if text1[i] == text2[j]:
+                dp[(i, j)] = 1 + dfs(i+1, j + 1)
+            else:
+                dp[(i, j)] = max(
+                    dfs(i, j + 1),
+                    dfs(i+1, j)
+                )
+            return dp[(i, j)]
+        return dfs(0, 0)
