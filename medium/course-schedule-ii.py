628b813e3ff2090436ddf63b60b223a8725ef75b
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-06-29 22:37:17 +0530

    Update course-schedule-ii.py

--- medium/course-schedule-ii.py
+++ medium/course-schedule-ii.py
@@ -1,6 +1,39 @@
 # https://leetcode.com/problems/course-schedule-ii/
 
 
+from collections import defaultdict, deque
+
+# Kahn’s Algorithm (BFS)
+def findOrder(numCourses, prerequisites):
+    graph = defaultdict(list)
+    in_degree = [0] * numCourses
+
+    # Build graph and in-degree array
+    for course, prereq in prerequisites:
+        graph[prereq].append(course)
+        in_degree[course] += 1
+
+    # Start with courses that have no prerequisites
+    queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
+    order = []
+
+    while queue:
+        node = queue.popleft()
+        order.append(node)
+
+        for neighbor in graph[node]:
+            in_degree[neighbor] -= 1
+            if in_degree[neighbor] == 0:
+                queue.append(neighbor)
+
+    # If all courses taken, return the order
+    if len(order) == numCourses:
+        return order
+    else:
+        return []  # cycle detected → impossible
+
+
+
 def solution(numCourses, prerequisites):
     graph = { i: [] for i in range(numCourses) }
     for u, v in prerequisites:
