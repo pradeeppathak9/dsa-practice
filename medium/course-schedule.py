bd420539da3f9eeb659222371bc9b8ec4cedef8c
pradeeppathak9 <pradeeppathak9@gmail.com>
2025-06-29 22:36:14 +0530

    Update course-schedule.py

--- medium/course-schedule.py
+++ medium/course-schedule.py
@@ -1,5 +1,35 @@
 # https://leetcode.com/problems/course-schedule/
 
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+
+        state = [0] * numCourses  # 0 = unvisited, 1 = visiting, 2 = visited
+
+        def hasCycle(node):
+            if state[node] == 1:
+                return True  # found a cycle
+            if state[node] == 2:
+                return False  # already checked, no cycle here
+
+            state[node] = 1  # mark as visiting
+            for neighbor in graph[node]:
+                if hasCycle(neighbor):
+                    return True
+
+            state[node] = 2  # mark as visited
+            return False
+
+        for i in range(numCourses):
+            if hasCycle(i):
+                return False
+
+        return True
+
+
 def solution(numCourses, prerequisites):
     graph = { i: [] for i in range(numCourses) }
     for u, v in prerequisites:
