bc9c5937eac3e2df98c1dda90a5c72d337cea0a2
pradeeppathak9 <pradeeppathak9@gmail.com>
2026-07-30 22:49:25 +0530

    Create insert-interval.py

---
+++ neetcode/intervals/insert-interval.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/insert-interval/
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        (s1, e1) = newInterval
+        
+        i = 0
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+            i += 1
+        res.append(newInterval)
+
+        while i < len(intervals):
+            res.append(intervals[i])
+            i += 1
+        return res
+
+        
+         
