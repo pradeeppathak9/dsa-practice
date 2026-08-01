0588306e21b414bf75b5f1fb04e1b90ebf160480
pradeeppathak9 <pradeeppathak9@gmail.com>
2026-08-01 17:24:33 +0530

    Update course-schedule-iv.py

--- neetcode/graphs/course-schedule-iv.py
+++ neetcode/graphs/course-schedule-iv.py
@@ -19,6 +19,39 @@ class Solution:
         return [reachable[u][v] for u, v in queries]
 
 
+# BFS
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = defaultdict(list)
+        for a, b in prerequisites:
+            graph[a].append(b)
+        
+        reachable = [set() for _ in range(numCourses)]
+
+        for start in range(numCourses):
+            queue = deque(graph[start])
+            visited = set(graph[start])
+            while queue:
+                node = queue.popleft()
+                reachable[start].add(node)
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append(neighbor)
+
+
+        return [v in reachable[u] for u, v in queries]
+
+
+
+
+
+
+
+
+
+        
+
 
         
 
